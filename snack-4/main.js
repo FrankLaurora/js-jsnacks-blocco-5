8144b21e92081fcd8373290a73ae398ba1b95c94
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-08 12:56:53 +0200

    Add snack 4

---
+++ snack-4/main.js
@@ -0,0 +1,25 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di
+//     elementi) prendendo alternativamente gli elementi da uno e dall’altro
+//     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+var mergedArr = [];
+
+function arrayMerger(lettere, numeri) {
+    if(lettere.length != numeri.length) {
+        alert("Questa funzione accetta solo array con la stessa lunghezza");
+    } else {
+        for(let i = 0; i < lettere.length; i++) {
+            mergedArr.push(lettere[i]);
+            mergedArr.push(numeri[i]);
+        }
+
+        return mergedArr;
+    }
+
+}
+
+var lettere = ["a", "b", "c"];
+
+var numeri = [1, 2, 3];
+
+console.log(arrayMerger(lettere, numeri));
