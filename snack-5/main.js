ce023b6017df122981f51c1615016fc0961b7274
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-09 11:08:26 +0200

    Fix returned array declared into pickElements

--- snack-5/main.js
+++ snack-5/main.js
@@ -4,13 +4,13 @@
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione
 // compresa tra “a” e “b”
 
-var partialArray = [];
-
 function pickElements(num1, num2, arr) {
     //effettuo un controllo per verificare che num1 e num2 contengano valori validi per la lunghezza di arr e maggiori di 0 (esprimo la posizione in forma umana)
     if(num1 > arr.length || num2 > arr.length || num1 == 0 || num2 == 0) {
         return alert(`Attenzione! Il tuo array contiene ${arr.length} elementi. Inserisci due numeri tra 1 e ${arr.length}.`);
     } else {
+        var partialArray = [];
+
         //prendo in considerazione il caso in cui l'utente inserisca la posizione maggiore come primo argomento della funzione
         if(num1 > num2) {
             for(let i = num2 - 1; i < num1; i++) {
@@ -30,6 +30,4 @@ function pickElements(num1, num2, arr) {
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-pickElements(7, 3, numbers);
-
-console.log(partialArray);
+console.log(pickElements(7, 3, numbers));
